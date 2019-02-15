570649d85ee51249009edd1bca3042c195c0868a
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-15 14:07:03 -0500

    Adding css

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import "./App.css";
 
+// const App = function () {
+//   return (<div className="App">
+//     Hola John!
+//   </div>);
+// };
 
 const App = () =>
   (<div className="App">
-    Hola John!
-  </div>);
+    <h1>Followers</h1>
+  </div>
+);
+
+
 
 export default App;
