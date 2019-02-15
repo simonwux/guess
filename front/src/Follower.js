57c7f347168ab66199408dad7051cd8552ba2bd7
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-15 14:35:19 -0500

    Now you can vote for a follower

--- front/src/Follower.js
+++ front/src/Follower.js
@@ -1,14 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Follower extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      votes :0
+    };
+
+    this.onVote = this.onVote.bind(this);
+  }
+
+  onVote() {
+    this.setState({
+      votes: this.state.votes + 1
+    });
+  }
+
   render() {
     return (
       <div className="follower">
         Follower:
         {this.props.follower}
-        <button><span role="img">🤙</span></button>
-        Votes : {0}
+        <button onClick={this.onVote}>
+          <span role="img">🤙</span>
+        </button>
+         Votes : {this.state.votes}
       </div>
     );
   }
