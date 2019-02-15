9da942628b896346bf6d927720736da7f769b7cc
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-15 14:13:21 -0500

    Rendering some data

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,31 @@ import React from "react";
 
 import "./App.css";
 
+
+
+
+
 // const App = function () {
 //   return (<div className="App">
 //     Hola John!
 //   </div>);
 // };
 
-const App = () =>
-  (<div className="App">
+const App = () => {
+  let data = ["John", "Mafe", "Suhas"];
+
+
+  return (<div className="App">
     <h1>Followers</h1>
+
+    { data.map((f,i) =>
+      <div key={i}>{f}</div>
+    )}
+
+    <div>Made by John with ♥️</div>
   </div>
-);
+  );
+};
 
 
 
