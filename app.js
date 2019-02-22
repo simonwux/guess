6a1d7b0b3b5f5a3c8b643a973a3e91371407a7f1
Simon <simonwux@126.com>
2019-02-22 14:48:35 -0800

    try to deploy on port 3001

--- app.js
+++ app.js
@@ -17,9 +17,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "front/build")));
 
 app.use("/", indexRouter);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
